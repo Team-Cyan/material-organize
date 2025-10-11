baab718f358e25a9b99b199daa33305aace5fb7d
Lancer Chen <lancer@Mac-mini.local>
2025-10-11 11:39:39 +0800

    Init project

---
+++ main.py
@@ -0,0 +1,7 @@
+from utils.file_helper import FileHelper
+
+if __name__ == '__main__':
+    target: str = "20250920"
+    source = FileHelper("/Users/lancer/import")
+    source.extract_raw(f"/Users/lancer/materials/{target}/photo")
+    source.extract_mp4(f"/Users/lancer/materials/{target}/video")
